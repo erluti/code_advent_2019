b7c3964efc50f5c68fb7e43a31711e19a1e52bdc
Philip Becker <philip.becker@oddball.io>
2019-12-04 09:44:38 -0500

    fix tests for new criteria

--- day4_spec.rb
+++ day4_spec.rb
@@ -7,8 +7,8 @@ require 'rspec'
 
 describe PasscodeVerifier do
   describe '#is_valid?' do
-    it 'should be true for 111111' do
-      expect(PasscodeVerifier.new(111111).is_valid?).to eq true
+    it 'should be false for 111111' do
+      expect(PasscodeVerifier.new(111111).is_valid?).to eq false
     end
     it 'should be false for 223450' do
       expect(PasscodeVerifier.new(223450).is_valid?).to eq false
@@ -19,16 +19,14 @@ describe PasscodeVerifier do
     it 'should be true for 123788' do
       expect(PasscodeVerifier.new(123788).is_valid?).to eq true
     end
-    context 'with part 2 criteria' do
-      it 'should be true for 112233' do
-        expect(PasscodeVerifier.new(112233).is_valid?).to eq true
-      end
-      it 'should be false for 123444' do
-        expect(PasscodeVerifier.new(123444).is_valid?).to eq false
-      end
-      it 'should be true for 111122' do
-        expect(PasscodeVerifier.new(111122).is_valid?).to eq true
-      end
+    it 'should be true for 112233' do
+      expect(PasscodeVerifier.new(112233).is_valid?).to eq true
+    end
+    it 'should be false for 123444' do
+      expect(PasscodeVerifier.new(123444).is_valid?).to eq false
+    end
+    it 'should be true for 111122' do
+      expect(PasscodeVerifier.new(111122).is_valid?).to eq true
     end
   end
 end
