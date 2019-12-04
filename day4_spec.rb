cba4463eefbd9dedde3064a88575c98e81cb8379
Philip Becker <philip.becker@oddball.io>
2019-12-04 09:34:23 -0500

    part 2 criteria as tests

--- day4_spec.rb
+++ day4_spec.rb
@@ -19,5 +19,16 @@ describe PasscodeVerifier do
     it 'should be true for 123788' do
       expect(PasscodeVerifier.new(123788).is_valid?).to eq true
     end
+    context 'with part 2 criteria' do
+      it 'should be true for 112233' do
+        expect(PasscodeVerifier.new(112233).is_valid?).to eq true
+      end
+      it 'should be false for 123444' do
+        expect(PasscodeVerifier.new(123444).is_valid?).to eq false
+      end
+      it 'should be true for 111122' do
+        expect(PasscodeVerifier.new(111122).is_valid?).to eq true
+      end
+    end
   end
 end
