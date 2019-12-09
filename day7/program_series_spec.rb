e9ae4ea3b1d56d51bb1c52e67be130ae7a9f1c71
Philip Becker <philip.becker@oddball.io>
2019-12-09 09:52:13 -0500

    Spec for example 2

--- day7/program_series_spec.rb
+++ day7/program_series_spec.rb
@@ -1,9 +1,9 @@
 require './program_series.rb'
 require 'rspec'
 
-RSpec.configure do |c|
-  c.filter_run_including :focus => true
-end
+# RSpec.configure do |c|
+#   c.filter_run_including :focus => true
+# end
 
 describe ProgramSeries do
   it 'should return 43210 (from phase setting sequence 4,3,2,1,0) for 3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0' do
@@ -33,7 +33,7 @@ describe ProgramSeries do
     end
   end
   describe 'with feedback loop' do
-    fdescribe 'example 1' do
+    describe 'example 1' do
       let(:intcode) { '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5' }
       let(:sequence) { [9,8,7,6,5] }
       let(:value) { 139629729 }
@@ -43,7 +43,21 @@ describe ProgramSeries do
       end
       it "should return [9,8,7,6,5] for .find_max_series" do
         program = ProgramSeries.find_max_series(intcode, feedback_loop: true)
-        expect(program.sequence).to eq [9,8,7,6,5]
+        expect(program.sequence).to eq sequence
+      end
+    end
+
+    describe 'example 2' do
+      let(:intcode) { '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10' }
+      let(:sequence) { [9,7,8,5,6] }
+      let(:value) { 18216 }
+      it "should return 18216 with sequence [9,7,8,5,6]" do
+        program_series = ProgramSeries.new(intcode, sequence)
+        expect(program_series.run(true)).to eq value
+      end
+      it "should return [9,7,8,5,6] for .find_max_series" do
+        program = ProgramSeries.find_max_series(intcode, feedback_loop: true)
+        expect(program.sequence).to eq sequence
       end
     end
 
