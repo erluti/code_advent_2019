663d962d9c56fa0e7d45d8b60c81ca724f94d591
Philip Becker <philip.becker@oddball.io>
2020-01-07 15:13:31 -0500

    start thinking about day16

---
+++ day16.rb
@@ -0,0 +1,9 @@
+# num.abs%10 will get last digit of num, regardless of pos/neg
+
+# create "phaser" that will take an input and return the output, contains the base pattern internally
+# examples will become tests
+
+if __FILE__ == $0
+  input = DATA.readline
+end
+__END__
