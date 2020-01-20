6f63bdc5a1c275fa0c5c8453130ad2d25f940df4
Philip Becker <philip.becker@oddball.io>
2020-01-20 10:35:12 -0500

    all examples from part1 as tests

--- day18_spec.rb
+++ day18_spec.rb
@@ -12,5 +12,61 @@ describe VaultMap do
       vault = VaultMap.new(map)
       expect(vault.steps).to eq 8
     end
+
+    it 'should find 86 steps for the second example' do
+      map = <<~MAP_INPUT
+        ########################
+        #f.D.E.e.C.b.A.@.a.B.c.#
+        ######################.#
+        #d.....................#
+        ########################
+      MAP_INPUT
+      vault = VaultMap.new(map)
+      expect(vault.steps).to eq 86
+    end
+
+    it 'should find 132 steps for the third example' do
+      map = <<~MAP_INPUT
+        ########################
+        #...............b.C.D.f#
+        #.######################
+        #.....@.a.B.c.d.A.e.F.g#
+        ########################
+      MAP_INPUT
+      vault = VaultMap.new(map)
+      expect(vault.steps).to eq 132
+    end
+
+    it 'should find 136 steps for the fourth example' do
+      map = <<~MAP_INPUT
+        #################
+        #i.G..c...e..H.p#
+        ########.########
+        #j.A..b...f..D.o#
+        ########@########
+        #k.E..a...g..B.n#
+        ########.########
+        #l.F..d...h..C.m#
+        #################
+      MAP_INPUT
+      vault = VaultMap.new(map)
+      expect(vault.steps).to eq 136
+    end
+
+    it 'should find 81 steps for the fifth example' do
+      map = <<~MAP_INPUT
+        #################
+        #i.G..c...e..H.p#
+        ########.########
+        #j.A..b...f..D.o#
+        ########@########
+        #k.E..a...g..B.n#
+        ########.########
+        #l.F..d...h..C.m#
+        #################
+      MAP_INPUT
+      vault = VaultMap.new(map)
+      expect(vault.steps).to eq 81
+    end
   end
 end
